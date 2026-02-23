7d9890496337fe3d983b06dd1061a4ebc01b5bcc
kaikoga <info@mail.kaikoga.net>
2026-02-23 20:14:50 +0900

    force override references and see what happens

--- Runtime/Ablet/Hooks/IAbletEditorOnly.cs
+++ Runtime/Ablet/Hooks/IAbletEditorOnly.cs
@@ -2,6 +2,7 @@ namespace Ablet.Hooks
 {
     public interface IAbletEditorOnly
 #if ABLET_VRCSDK3_AVATARS
+    // FIXME: remove VRCSDK dll reference from inheritors 
 : VRC.SDKBase.IEditorOnly
 #endif
     {
