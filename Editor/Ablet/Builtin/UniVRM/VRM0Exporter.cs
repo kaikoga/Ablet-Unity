2bb4284e15800f6bb57aaf024b5b2b31e8dc6c78
kaikoga <info@mail.kaikoga.net>
2026-06-07 11:01:29 +0900

    extract VRMExportSettings

--- Editor/Ablet/Builtin/UniVRM/VRM0Exporter.cs
+++ Editor/Ablet/Builtin/UniVRM/VRM0Exporter.cs
@@ -1,22 +1,30 @@
+using System;
 using UnityEngine;
 using VRM;
+using Object = UnityEngine.Object;
 using VRMEditorExporter = Ablet.Builtin.UniVRM.Exporter.VRMEditorExporter;
 
 namespace Ablet.Builtin.UniVRM
 {
     public static class VRM0Exporter
     {
+        [Obsolete]
         public static byte[] ExportVRM0(VRMMeta vrmMeta)
         {
             var settings = ScriptableObject.CreateInstance<VRMExportSettings>();
             try
             {
-                return VRMEditorExporter.Export(vrmMeta.gameObject, vrmMeta.Meta, settings);
+                return ExportVRM0(vrmMeta, settings);
             }
             finally
             {
                 Object.DestroyImmediate(settings);
             }
+        }
+        
+        public static byte[] ExportVRM0(VRMMeta vrmMeta, VRMExportSettings settings)
+        {
+            return VRMEditorExporter.Export(vrmMeta.gameObject, vrmMeta.Meta, settings);
         }
     }
 }
--- Editor/Ablet/Builtin/UniVRM10/VRM1Exporter.cs
+++ Editor/Ablet/Builtin/UniVRM10/VRM1Exporter.cs
@@ -1,44 +1,51 @@
+using System;
 using UniGLTF;
 using UnityEngine;
 using UniVRM10;
+using Object = UnityEngine.Object;
 
 namespace Ablet.Builtin.UniVRM10
 {
     public static class VRM1Exporter
     {
+        [Obsolete]
         public static byte[] ExportVRM1(Vrm10Instance vrm10Instance)
         {
             var settings = ScriptableObject.CreateInstance<VRM10ExportSettings>();
-            using var arrayManager = new NativeArrayManager();
-
             try
             {
-                // Based on Vrm10Exporter.Export()
-
-                // ヒエラルキーからジオメトリーを収集
-                var converter = new ModelExporter();
-                var model = converter.Export(settings.MeshExportSettings, arrayManager, vrm10Instance.gameObject);
-
-                // 右手系に変換
-                model.ConvertCoordinate(VrmLib.Coordinates.Vrm1);
-
-                var exporter10 = new Vrm10Exporter(
-                    settings.MeshExportSettings,
-                    // Use RuntimeTextureSerializer, because EditorTextureSerializer may destroy asset import settings
-                    textureSerializer: new RuntimeTextureSerializer()
-                );
-                var option = new VrmLib.ExportArgs
-                {
-                    sparse = settings.MorphTargetUseSparse,
-                };
-                exporter10.Export(vrm10Instance.gameObject, model, converter, option, vrm10Instance.Vrm.Meta);
-
-                return exporter10.Storage.ToGlbBytes();
+                return ExportVRM1(vrm10Instance, settings);
             }
             finally
             {
                 Object.DestroyImmediate(settings);
             }
+        }
+        
+        public static byte[] ExportVRM1(Vrm10Instance vrm10Instance, VRM10ExportSettings settings)
+        {
+            using var arrayManager = new NativeArrayManager();
+            // Based on Vrm10Exporter.Export()
+
+            // ヒエラルキーからジオメトリーを収集
+            var converter = new ModelExporter();
+            var model = converter.Export(settings.MeshExportSettings, arrayManager, vrm10Instance.gameObject);
+
+            // 右手系に変換
+            model.ConvertCoordinate(VrmLib.Coordinates.Vrm1);
+
+            var exporter10 = new Vrm10Exporter(
+                settings.MeshExportSettings,
+                // Use RuntimeTextureSerializer, because EditorTextureSerializer may destroy asset import settings
+                textureSerializer: new RuntimeTextureSerializer()
+            );
+            var option = new VrmLib.ExportArgs
+            {
+                sparse = settings.MorphTargetUseSparse,
+            };
+            exporter10.Export(vrm10Instance.gameObject, model, converter, option, vrm10Instance.Vrm.Meta);
+
+            return exporter10.Storage.ToGlbBytes();
         }
     }
 }
